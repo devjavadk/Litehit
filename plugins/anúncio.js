0955c5fabedc499db8a101d11cfc0b9a5a440226
devjavadk <76820782+devjavadk@users.noreply.github.com>
2021-01-07 11:46:01 -0300

    Update anúncio.js

--- plugins/anúncio.js
+++ plugins/anúncio.js
@@ -1,4 +1,5 @@
-case 'chamada':
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+  conn.reply(m.chat, `
 					if (!isGroup) return reply(mess.only.group)
 					if (!isGroupAdmins) return reply(mess.only.admin)
 					members_id = []
@@ -10,3 +11,17 @@ case 'chamada':
 					}
 					mentions(teks, members_id, true)
 					break
+handler.command = /^(anúncio|anúnciar|\?)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
