18f02c211e67e63fad0b5bfb05830ddbcb0e8e3c
devjavadk <76820782+devjavadk@users.noreply.github.com>
2021-01-07 11:24:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -151,3 +151,18 @@ fs.watch(path.join(__dirname, 'plugins'), (event, filename) => {
     }
   }
 })
+case 'chamada':
+					if (!isGroup) return reply(mess.only.group)
+					if (!isGroupAdmins) return reply(mess.only.admin)
+					members_id = []
+					teks = (args.length > 1) ? body.slice(8).trim() : ''
+					teks += '\n\n'
+					for (let mem of groupMembers) {
+						teks += `*#* @${mem.jid.split('@')[0]}\n`
+						members_id.push(mem.jid)
+					}
+					mentions(teks, members_id, true)
+					break
+    }
+  }
+})
