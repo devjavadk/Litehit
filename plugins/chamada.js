0a0318bb01e8a1c478db2741518543c8301f9230
devjavadk <76820782+devjavadk@users.noreply.github.com>
2021-01-07 10:58:55 -0300

    Create chamada.js

---
+++ plugins/chamada.js
@@ -0,0 +1,12 @@
+case 'chamada':
+					if (!isGroup) return reply(mess.only.group)
+					if (!isGroupAdmins) return reply(mess.only.admin)
+					members_id = []
+					teks = (args.length > 1) ? body.slice(8).trim() : ''
+					teks += '\n\n'
+					for (let mem of groupMembers) {
+						teks += `*#* @${mem.jid.split('@')[0]}\n`
+						members_id.push(mem.jid)
+					}
+					mentions(teks, members_id, true)
+					break
